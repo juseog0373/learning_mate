0494391958acd0897bcbf43f415ebc4077d09894
jskim <juseog0373@naver.com>
2024-04-03 16:32:28 +0900

    add: user login, join - logout

--- src/main/java/daelim/learning/config/SecurityConfig.java
+++ src/main/java/daelim/learning/config/SecurityConfig.java
@@ -1,17 +1,39 @@
-//package daelim.learning.config;
-//
-//import jakarta.persistence.EntityManager;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class SecurityConfig {
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
-//
+package daelim.learning.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    // BCryptPasswordEncoder 빈 정의
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private static final String[] WHITE_LIST_URL = {
+            "/",
+            "/user/**", // 요청 허가 url 목록
+            "/board/**",
+    };
+
+    // SecurityFilterChain 빈 정의
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // CSRF 보호 기능을 비활성화하려면 아래 주석을 해제합니다.
+                .csrf().disable()
+                // 모든 사용자가 접근할 수 있도록 /
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
--- src/main/java/daelim/learning/user/UserController.java
+++ src/main/java/daelim/learning/user/UserController.java
@@ -2,45 +2,49 @@ package daelim.learning.user;
 
 import daelim.learning.user.dto.JoinRequest;
 import daelim.learning.user.dto.LoginRequest;
-import lombok.Getter;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.mapping.Join;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/login")
-    public String loginForm() {
+    public String loginForm(Model model) {
+        model.addAttribute("loginRequest", new LoginRequest());
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute LoginRequest request) {
-        userService.login(request);
-        return "";
+    public String login(@ModelAttribute("loginRequest") LoginRequest loginRequest, HttpSession session, RedirectAttributes redirectAttributes) {
+        userService.login(loginRequest, session, redirectAttributes);
+        return "redirect:/";
     }
 
-    @GetMapping("/signup")
-    public String signupForm() {
-        return "signup";
+    @GetMapping("/join")
+    public String joinForm(Model model, JoinRequest joinRequest) {
+        model.addAttribute("joinRequest", joinRequest);
+        return "join";
     }
 
-    @PostMapping("/signup")
-    public String signup(@ModelAttribute JoinRequest request, BindingResult bindingResult) {
-        if(userService.isUserIdDuplicate(request.getUserId())) {
-            bindingResult.addError(new FieldError("joinRequest", "userId", "중복된 아이디입니다."));
-        }
-        userService.signup(request);
-        return "";
+    @PostMapping("/join")
+    public String join(@ModelAttribute("joinRequest") JoinRequest request, BindingResult bindingResult) {
+        String url = userService.join(request, bindingResult);
+        return url;
     }
 }
