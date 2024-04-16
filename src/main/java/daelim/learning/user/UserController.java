487ff4228134de40632f5c586617cded8dcb7191
ahniksoo <myway010718@naver.com>
2024-04-16 14:15:21 +0900

    add: 유저 프로필 편집 추가

--- src/main/java/daelim/learning/user/UserController.java
+++ src/main/java/daelim/learning/user/UserController.java
@@ -2,6 +2,7 @@ package daelim.learning.user;
 
 import daelim.learning.user.dto.JoinRequest;
 import daelim.learning.user.dto.LoginRequest;
+import daelim.learning.user.dto.UserUpdateRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -57,4 +58,33 @@ public class UserController {
         model.addAttribute("myBoardList", userService.myBoardList(userNo));
         return "user/detail";
     }
+    @GetMapping("/edit")
+    public String editForm(Model model, HttpSession session, UserUpdateRequest userUpdateRequest) {
+        Long userNo = (Long) session.getAttribute("userNo");
+        if (userNo == null) {
+            // 사용자가 로그인하지 않은 경우 로그인 페이지로 리다이렉션
+            return "redirect:/user/login";
+        }
+        User user = userService.userDetail(userNo);
+
+        model.addAttribute("user", user);
+        model.addAttribute("updateRequest", userUpdateRequest); // UserUpdateRequest 추가
+        return "/user/edit-profile";
+    }
+
+    @PostMapping("/edit")
+    public String editProfile(@ModelAttribute("updateRequest") UserUpdateRequest updateRequest, HttpSession session, RedirectAttributes redirectAttributes) {
+        Long userNo = (Long) session.getAttribute("userNo");
+        if (userNo == null) {
+            // 사용자가 로그인하지 않은 경우 로그인 페이지로 리다이렉션
+            return "redirect:/user/login";
+        }
+        userService.editProfile(userNo, updateRequest);
+        session.setAttribute("userName", updateRequest.getUserName());  //update한 내용을 session에 저장
+
+        redirectAttributes.addAttribute("userNo", userNo);
+
+        return "redirect:/user/detail/{userNo}";
+    }
+
 }
